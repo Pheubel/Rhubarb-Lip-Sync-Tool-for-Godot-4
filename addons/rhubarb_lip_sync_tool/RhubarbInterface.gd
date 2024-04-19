abeb0fedea7c31dac3c51780e46e751d36da593e
Pheubel <luca.guagliardo@live.nl>
2024-04-19 23:15:44 +0200

    added function for clearing directories

--- addons/rhubarb_lip_sync_tool/RhubarbInterface.gd
+++ addons/rhubarb_lip_sync_tool/RhubarbInterface.gd
@@ -2,13 +2,24 @@
 extends Node
 class_name RhubarbInterfaceNode
 
-const output_directory := "res://.godot/rhubarb/output"
-const dialog_directory := "res://.godot/rhubarb/dialog"
+const output_directory_path := "res://.godot/rhubarb/output"
+const dialog_directory_path := "res://.godot/rhubarb/dialog"
 
 func _enter_tree() -> void:
 	print("Rhubarb Interface Node entered tree")
 
 func _ready() -> void:
+	#run_rhubarb("res://what-the-hell-meme-sound-effect.ogg")
+	
+	var task := get_mouth_shape_data_async("res://what-the-hell-meme-sound-effect.ogg")
+	
+	print("started rubarb task")
+	
+	await task.task_completed
+	var data := task.get_result() as RhubarbData 
+	
+	print(data.samples)
+	
 	pass
 
 ## Start Rhubarb to sample the audio file. Returns the path to the output file.
@@ -25,7 +36,7 @@ func run_rhubarb(input_file: String, dialog_file: String = "", recognizer: Strin
 		push_error("[Rhubarb Lip Sync Tool]: File '%s' does not exist." % input_file)
 		return ""
 	
-	var output_path = output_directory.path_join("%s%s" % [input_file.get_file().trim_suffix(input_file.get_extension()), output_format])
+	var output_path = output_directory_path.path_join("%s%s" % [input_file.get_file().trim_suffix(input_file.get_extension()), output_format])
 	
 	var editor_settings := EditorInterface.get_editor_settings()
 	
@@ -100,9 +111,35 @@ func get_mouth_shape_data_async(input_file: String, dialog_file: String = "", re
 	
 	return task
 
-func _ensure_directories_exist() -> void:
-	if !DirAccess.dir_exists_absolute(output_directory):
-		DirAccess.make_dir_recursive_absolute(output_directory)
+## Removes all files from the output and dialog directories
+func clear_directory() -> void:
+	_ensure_directories_exist()
 	
-	if !DirAccess.dir_exists_absolute(dialog_directory):
-		DirAccess.make_dir_recursive_absolute(dialog_directory)
+	var output_dir := DirAccess.open(output_directory_path)
+	if output_dir:
+		output_dir.list_dir_begin()
+		
+		var next_item := output_dir.get_next()
+		while !next_item.is_empty():
+			if !output_dir.current_is_dir():
+				output_dir.remove(next_item)
+			
+			next_item = output_dir.get_next()
+	
+	var dialog_dir := DirAccess.open(dialog_directory_path)
+	if dialog_dir:
+		dialog_dir.list_dir_begin()
+		
+		var next_item := dialog_dir.get_next()
+		while !next_item.is_empty():
+			if !dialog_dir.current_is_dir():
+				dialog_dir.remove(next_item)
+			
+			next_item = dialog_dir.get_next()
+
+func _ensure_directories_exist() -> void:
+	if !DirAccess.dir_exists_absolute(output_directory_path):
+		DirAccess.make_dir_recursive_absolute(output_directory_path)
+	
+	if !DirAccess.dir_exists_absolute(dialog_directory_path):
+		DirAccess.make_dir_recursive_absolute(dialog_directory_path)
