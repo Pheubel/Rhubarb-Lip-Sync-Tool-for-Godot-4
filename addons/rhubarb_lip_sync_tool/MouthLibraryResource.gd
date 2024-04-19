29472b274cacccbcce861310487c016c3a85177a
Pheubel <luca.guagliardo@live.nl>
2024-04-19 23:14:37 +0200

    renamed and added comments

---
+++ addons/rhubarb_lip_sync_tool/MouthLibraryResource.gd
@@ -0,0 +1,30 @@
+## A library of Preston Blair mouth shapes that will be used for animating the mouth.
+extends Resource
+class_name MouthLibraryResource
+
+## Closed mouth used for a resting position.
+@export var rest_shape: Texture2D
+
+## Closed mouth for the "P", "B", and "M" sounds.
+@export var mbp_shape: Texture2D
+
+## Slightly open mouth with teeth against the bottom lips used for the "F" and "V" sounds.
+@export var fv_shape: Texture2D
+
+## Open mouth used for "O" sounds.
+@export var o_shape: Texture2D
+
+## Puckered mouth used for the "U", "OW" and "W" sounds.
+@export var u_shape: Texture2D
+
+## Slightlyy opened mouth with clenched teeth used for "E" and "AE" sounds.
+@export var e_shape: Texture2D
+
+## Open mouth used for "L" sounds.
+@export var l_shape: Texture2D
+
+## Open mouth used for the "A" sounds.
+@export var ai_shape: Texture2D
+
+## Slightly opened mouth used for most of the constonants and some vowels.
+@export var etc_shape: Texture2D
